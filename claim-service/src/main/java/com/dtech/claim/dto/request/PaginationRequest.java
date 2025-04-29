325b1cb3d7cc480a8cb1cffa8a0dce3c7be2ae24
himaljdev <himalj@dtechlk.com>
2025-04-29 12:20:35 +0530

    Added paginator modifications

--- claim-service/src/main/java/com/dtech/claim/dto/request/PaginationRequest.java
+++ claim-service/src/main/java/com/dtech/claim/dto/request/PaginationRequest.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Sort;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaginationRequest<T> extends ChannelRequestValidatorDTO {
-    private Integer page = 1;
+    private Integer page = 0;
     private Integer size = 10;
     private String sortColumn = "lastModifiedDate";
     private Sort.Direction sortDirection = Sort.Direction.DESC;
